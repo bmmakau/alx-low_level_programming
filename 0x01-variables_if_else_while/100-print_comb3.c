ab087f019a2118d57d93de936f9ca3a996f74522
BM Makau <bmmakau74@gmail.com>
2022-10-16 13:12:26 -0700

    C variables task 10 correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,26 +2,27 @@
 
 /**
   * main - Entry point
-  * Description: prints 2 digit combo that do not repeat
+  * Description: print combination of #s with nonrepeating values
   * Return: 0
   */
 int main(void)
 {
 	int i, j;
 
-	i = j = '0';
-
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			putchar(i);
-			putchar(j);
-
-			if ((i != '9') || (i == '9' && j != '9'))
+			if (i < j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+
+				if (i != '8' || (i == '8' && j != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
