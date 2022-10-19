f4e414c66ba9b72b1b18a6af7d98ba1d991b320c
BM Makau <bmmakau74@gmail.com>
2022-10-19 03:08:37 -0700

    C functions and nested loops task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  * times_table - Prints the 9 times table, beginning with 0
+  *
+  * Return: void
+  */
+void times_table(void)
+{
+	int x, y, z, a, b;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				a = z % 10;
+				b = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(a + '0');
+				_putchar(b + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
