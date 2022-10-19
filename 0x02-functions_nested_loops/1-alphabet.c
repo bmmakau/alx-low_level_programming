c72d2808a628638a6494ffb5fdb08dfd2f1064e3
BM Makau <bmmakau74@gmail.com>
2022-10-19 00:23:07 -0700

    C functions and nested loops task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets
+ * Description: print a - z
+ * Return: no return type
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c < 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar(\n);
+}
+/**
+ * main - Entry point
+ * Description: print a - z
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
