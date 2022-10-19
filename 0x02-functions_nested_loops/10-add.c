e86fc8a599f88c362c76dc0bb5363c9363fe1ba5
BM Makau <bmmakau74@gmail.com>
2022-10-19 03:17:25 -0700

    C functions and nested loops task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+  * add - Adds two integers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: sum of 2 integers
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
