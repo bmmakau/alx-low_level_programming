1824e59dd005d5e248f183d2552bc21f9a17fb13
BM Makau <bmmakau74@gmail.com>
2022-10-26 11:47:20 -0700

    pointers, arrays and strings task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_rev - Prints string in reverse order
+  * @s: string to print
+  *
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
+	_putchar('\n');
+}
