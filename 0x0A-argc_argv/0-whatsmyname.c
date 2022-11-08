28c6642a80dbf7349d33b283e11c24381b597074
BM Makau <bmmakau74@gmail.com>
2022-11-08 04:06:07 -0800

    C argc, argv task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints its name.
+ * @argc: Argument count.
+ * @argv: Array of args.
+ *
+ * Return: Zero.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
